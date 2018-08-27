Changes: - The BMFMC low-fidelity push-forward is now computed for CBayes - Fixed the KL calculations between two distributions - BMFMC stats are now calculated via distribution objects

pycharm/cbayes-bmfmc-oo/BMFMC.py
@@ -29,10 +29,10 @@ class BMFMC:
 
     def apply_bmfmc_framework(self):
 
-        for i in range(self.n_models-1):
+        for i in range(self.n_models - 1):
 
             lf_model = self.models[i]
-            hf_model = self.models[i+1]
+            hf_model = self.models[i + 1]
 
             # 1) Evaluate the lowest-fidelity model
             if i == 0:
@@ -105,6 +105,17 @@ class BMFMC:
             hf_rv_samples = lf_model.rv_samples_pred[indices, :]
             hf_model.set_rv_samples(hf_rv_samples)
 
+        elif self.training_set_strategy == 'adaptive':
+
+            print('Not implemented yet')
+            exit()
+
+            # 1) Figure out where the model has already been evaluated (model.rv_samples)
+            # 2) Figure out how many more evaluations are desired
+            # 3) Add new evaluations points according to 'support_covering' strategy (append to model.rv_samples)
+            # 4) Evaluate the model at these points (append to model.model_evals)
+            # 5) Check the KL between old and updated model
+
         else:
             print('Unknown training set selection strategy.')
             exit()
@@ -145,6 +156,10 @@ class BMFMC:
 
         return self.models[-1].model_evals_pred
 
+    def get_low_fidelity_samples(self):
+
+        return self.models[0].model_evals_pred
+
     def calculate_mc_reference(self):
 
         self.mc_model = Model(eval_fun=self.models[-1].eval_fun, n_evals=self.models[0].n_evals,
@@ -161,42 +176,33 @@ class BMFMC:
         print('########### BMFMC statistics ###########')
         print('')
         if mc and self.mc_model != 0:
-            print('MC mean:\t\t\t\t\t\t%f' % np.mean(self.mc_model.model_evals_pred))
-            print('MC std:\t\t\t\t\t\t\t%f' % np.std(self.mc_model.model_evals_pred))
+            print('MC mean:\t\t\t\t\t\t%f' % self.mc_model.distribution.mean())
+            print('MC std:\t\t\t\t\t\t\t%f' % self.mc_model.distribution.std())
             print('')
-            print('MC-BMFMC KL:\t\t\t\t\t%f' % np.mean(
-                np.log(self.models[-1].distribution.kernel_density(np.squeeze(self.models[-1].model_evals_pred)) /
-                       self.mc_model.distribution.kernel_density(np.squeeze(self.mc_model.model_evals_pred)))))
+            print('MC-BMFMC KL:\t\t\t\t\t%f' % self.mc_model.distribution.calculate_kl_divergence(
+                self.models[-1].distribution))
         elif mc and self.mc_model == 0:
             print('No Monte Carlo reference samples available. Call calculate_mc_reference() first.')
             exit()
         print('')
-        print('Low-fidelity mean:\t\t\t\t%f' % np.mean(self.models[0].model_evals_pred))
-        print('Low-fidelity std:\t\t\t\t%f' % np.std(self.models[0].model_evals_pred))
+        print('Low-fidelity mean:\t\t\t\t%f' % self.models[0].distribution.mean())
+        print('Low-fidelity std:\t\t\t\t%f' % self.models[0].distribution.std())
         print('')
-        kl_sum = 0.0
-        for i in range(self.n_models-2):
-            print('Mid-%d-fidelity mean:\t\t\t%f' % (int(i+1), np.mean(self.models[i+1].model_evals_pred)))
-            print('Mid-%d-fidelity std:\t\t\t\t%f' % (int(i+1), np.std(self.models[i+1].model_evals_pred)))
+        for i in range(self.n_models - 2):
+            print('Mid-%d-fidelity mean:\t\t\t%f' % (int(i + 1), self.models[i + 1].distribution.mean()))
+            print('Mid-%d-fidelity std:\t\t\t\t%f' % (int(i + 1), self.models[i + 1].distribution.std()))
             print('')
-            kl = np.mean(
-                np.log(self.models[i].distribution.kernel_density(np.squeeze(self.models[i].model_evals_pred)) /
-                       self.models[i + 1].distribution.kernel_density(np.squeeze(self.models[i + 1].model_evals_pred))))
+            kl = self.models[i].distribution.calculate_kl_divergence(self.models[i + 1].distribution)
             print('Relative information gain:\t\t%f' % kl)
             print('')
-            kl_sum += kl
-        print('High-fidelity mean:\t\t\t\t%f' % np.mean(self.models[-1].model_evals_pred))
-        print('High-fidelity std:\t\t\t\t%f' % np.std(self.models[-1].model_evals_pred))
+        print('High-fidelity mean:\t\t\t\t%f' % self.models[-1].distribution.mean())
+        print('High-fidelity std:\t\t\t\t%f' % self.models[-1].distribution.std())
         print('')
-        kl = np.mean(
-            np.log(self.models[-2].distribution.kernel_density(np.squeeze(self.models[-2].model_evals_pred)) /
-                   self.models[-1].distribution.kernel_density(np.squeeze(self.models[-1].model_evals_pred))))
-        kl_sum += kl
+        kl = self.models[-2].distribution.calculate_kl_divergence(self.models[-1].distribution)
         print('Relative information gain:\t\t%f' % kl)
-        print('Total information gain (calc):\t%f' % np.mean(
-            np.log(self.models[0].distribution.kernel_density(np.squeeze(self.models[0].model_evals_pred)) /
-                   self.models[-1].distribution.kernel_density(np.squeeze(self.models[-1].model_evals_pred)))))
-        print('Total information gain (sum):\t%f' % kl_sum)
+        print('')
+        print('Total information gain:\t\t\t%f' % self.models[0].distribution.calculate_kl_divergence(
+            self.models[-1].distribution))
         print('')
         print('########################################')
         print('')
@@ -208,11 +214,10 @@ class BMFMC:
         xmax = np.max([np.max(self.models[-1].model_evals_pred), np.max(self.models[0].model_evals_pred)])
 
         for i in range(self.n_models):
-
             # Plot
             color = 'C' + str(i)
             self.models[i].distribution.plot_kde(fignum=self.fignum, color=color, xmin=xmin, xmax=xmax,
-                                                  title='BMFMC - approximate distributions')
+                                                 title='BMFMC - approximate distributions')
 
         if mc and self.mc_model != 0:
             self.mc_model.distribution.plot_kde(fignum=self.fignum, color='k', linestyle='--',
@@ -235,11 +240,10 @@ class BMFMC:
         xmax = np.max([np.max(self.mc_model.model_evals_pred), np.max(self.models[0].model_evals_pred)])
 
         for i in range(self.n_models):
-
             # Plot
             color = 'C' + str(i)
             self.models[i].distribution.plot_kde(fignum=self.fignum, color=color, xmin=xmin, xmax=xmax,
-                                                  title='BMFMC - approximate distributions')
+                                                 title='BMFMC - approximate distributions')
 
         self.mc_model.distribution.plot_kde(fignum=self.fignum, color='k', linestyle='--',
                                             xmin=xmin, xmax=xmax, title='BMFMC - approximate distributions')
@@ -249,10 +253,9 @@ class BMFMC:
 
     def plot_regression_models(self):
 
-        for i in range(self.n_models-1):
-
+        for i in range(self.n_models - 1):
             lf_model = self.models[i]
-            hf_model = self.models[i+1]
+            hf_model = self.models[i + 1]
 
             regression_model = self.regression_models[i]
 
@@ -272,15 +275,14 @@ class BMFMC:
                                label='Training', title='BMFMC - regression model', xlabel=lf_model.rv_name,
                                ylabel=hf_model.rv_name)
 
-            plt.gcf().savefig('pngout/bmfmc_regression_model_' + str(i+1) + '.png', dpi=300)
+            plt.gcf().savefig('pngout/bmfmc_regression_model_' + str(i + 1) + '.png', dpi=300)
             self.fignum += 1
 
     def plot_joint_densities(self):
 
-        for i in range(self.n_models-1):
-
+        for i in range(self.n_models - 1):
             lf_model = self.models[i]
-            hf_model = self.models[i+1]
+            hf_model = self.models[i + 1]
 
             utils.plot_2d_contour(samples_x=np.squeeze(lf_model.model_evals_pred),
                                   samples_y=np.squeeze(hf_model.model_evals_pred),
@@ -289,5 +291,5 @@ class BMFMC:
                                   xlabel=lf_model.rv_name,
                                   ylabel=hf_model.rv_name)
 
-            plt.gcf().savefig('pngout/bmfmc_joint_dist_' + str(i+1) + '.png', dpi=300)
+            plt.gcf().savefig('pngout/bmfmc_joint_dist_' + str(i + 1) + '.png', dpi=300)
             self.fignum += 1

pycharm/cbayes-bmfmc-oo/CBayes.py
@@ -54,7 +54,9 @@ class CBayesPosterior:
         self.r = r
         self.acc_rate = idx.size / r.shape[0]
 
-        return self.p_prior.samples[idx], self.p_obs.samples[idx]
+        self.p_obs.samples = self.p_obs.samples[idx]
+
+        return self.p_prior.samples[idx], self.p_obs.samples
 
     # Create the posterior and its push-forward
     def setup_posterior_and_pf(self):
@@ -72,8 +74,6 @@ class CBayesPosterior:
                                       rv_transform=self.p_obs.rv_transform,
                                       label='Posterior-PF')
 
-        return self.p_post, self.p_post_pf
-
     # Print a bunch of output diagnostics
     def print_stats(self):
 
@@ -82,7 +82,7 @@ class CBayesPosterior:
         print('')
 
         # The rejection sampling acceptance rate
-        print('Acceptance rate:\t\t\t\t%f' % self.acc_rate )
+        print('Acceptance rate:\t\t\t\t%f' % self.acc_rate)
 
         # The posterior push-forward mean and std
         # (these should match the observed density)
@@ -91,8 +91,7 @@ class CBayesPosterior:
 
         # The KL between the push-forward of the posterior and the observed density
         # (this should be very close to zero)
-        print('Posterior-PF-Obs KL:\t\t\t%f' % np.mean(np.log(self.p_obs.kernel_density(np.squeeze(self.p_obs.samples)) /
-                                                self.p_post_pf.kernel_density(np.squeeze(self.p_obs.samples)))))
+        print('Posterior-PF-Obs KL:\t\t\t%f' % self.p_obs.calculate_kl_divergence(self.p_post_pf))
 
         # The posterior integral
         # (this should be very close to 1.0)
@@ -100,6 +99,8 @@ class CBayesPosterior:
 
         # The KL between posterior and prior (i.e. how informative is the data?)
         # (add a very small number to avoid taking log(0))
+        # This is done via r, because doing KDE for the prior / posterior densities can be hard when the number of
+        # random variables is large.
         print('Posterior-Prior KL:\t\t\t\t%f' % np.mean(self.r * np.log(self.r + 1e-10)))
 
         print('')
@@ -119,4 +120,7 @@ class CBayesPosterior:
         self.p_post_pf.plot_kde(fignum=fignum, color='C2', linestyle='--', xmin=xmin, xmax=xmax,
                                 title='CBayes')
 
-        plt.gcf().savefig('pngout/cbayes_dists.png', dpi=300)
+        if fignum == 2:
+            plt.gcf().savefig('pngout/cbayes_dists_lf.png', dpi=300)
+        else:
+            plt.gcf().savefig('pngout/cbayes_dists.png', dpi=300)

pycharm/cbayes-bmfmc-oo/main.py
@@ -28,9 +28,9 @@ pf_method = 'bmfmc'
 # Number of model evaluations in increasing fidelity (the lowest-fidelity model will always have n_samples evals)
 # Only lambda_p and ode_pp support more than one (i.e. arbitrary many) low-fidelity level
 # The number of models will thus be len(n_evals) + 1
-n_evals = [500, 100, 20]
-# n_evals = [100, 10]
-# n_evals = [10]
+# n_evals = [500, 100, 20]
+n_evals = [100, 10]
+# n_evals = [50]
 
 # Training set selection strategy (support_covering, sampling)
 training_set_strategy = 'support_covering'
@@ -40,9 +40,10 @@ regression_type = 'gaussian_process'
 
 # ---------------------------------------------------------- Todos ---------- #
 
-# todo: figure out why the KL between low- and high- model is equal to the sum of the KL between all intermediate ones
-# todo: compare high-fidelity with low-fidelity cbayes solutions
+# todo: implement distribution transformations to operate in unconstrained probability space only
+# todo: why is cbayes so robust wrt. the push-forward of the prior?
 # todo: implement adaptive training set selection procedure (requires some measure of information gain)
+# (uniform / LHS / sparse grids)
 # todo: add support for multiple QoIs (some parts are already there)
 # todo: implement other regression models
 
@@ -118,6 +119,7 @@ def get_prior_prior_pf_samples(n_samples):
 
             # Get prior push-forward samples
             prior_pf_samples = bmfmc.get_high_fidelity_samples()
+            prior_pf_lf_samples = bmfmc.get_low_fidelity_samples()
 
         else:
             print('Unknown push-forward method: %r' % pf_method)
@@ -187,6 +189,7 @@ def get_prior_prior_pf_samples(n_samples):
 
             # Get prior push-forward samples
             prior_pf_samples = bmfmc.get_high_fidelity_samples()
+            prior_pf_lf_samples = bmfmc.get_low_fidelity_samples()
 
         else:
             print('Unknown push-forward method: %r' % pf_method)
@@ -273,6 +276,7 @@ def get_prior_prior_pf_samples(n_samples):
 
             # Get prior push-forward samples
             prior_pf_samples = bmfmc.get_high_fidelity_samples()
+            prior_pf_lf_samples = bmfmc.get_low_fidelity_samples()
 
         else:
             print('Unknown push-forward method: %r' % pf_method)
@@ -282,7 +286,7 @@ def get_prior_prior_pf_samples(n_samples):
         print('Unknown model: %r' % model)
         exit()
 
-    return prior_samples, prior_pf_samples, obs_loc, obs_scale
+    return prior_samples, prior_pf_samples, obs_loc, obs_scale, prior_pf_lf_samples
 
 
 # --------------------------------------------------------------------------- #
@@ -295,7 +299,7 @@ if __name__ == '__main__':
     # Get samples from the prior, its push-forward and the observed density
     print('')
     print('Calculating the Prior push-forward ...')
-    prior_samples, prior_pf_samples, obs_loc, obs_scale = get_prior_prior_pf_samples(n_samples)
+    prior_samples, prior_pf_samples, obs_loc, obs_scale, prior_pf_lf_samples = get_prior_prior_pf_samples(n_samples)
 
     # Prior
     p_prior = Distribution(prior_samples, rv_name='$\lambda$', label='Prior', kde=False)
@@ -311,12 +315,24 @@ if __name__ == '__main__':
     # Posterior
     print('Evaluating the posterior ...')
     cbayes_post = CBayesPosterior(p_obs=p_obs, p_prior=p_prior, p_prior_pf=p_prior_pf)
-    p_post, p_post_pf = cbayes_post.setup_posterior_and_pf()
+    cbayes_post.setup_posterior_and_pf()
 
     # Output
     cbayes_post.print_stats()
 
     # Plot
     cbayes_post.plot_results(1)
+
+    # Low-fidelity comparison
+    if pf_method == 'bmfmc':
+        p_prior_pf_lf = Distribution(prior_pf_lf_samples, rv_name='$Q$', label='Prior-PF-LF')
+        obs_samples = np.random.randn(n_samples) * obs_scale + obs_loc
+        obs_samples = np.reshape(obs_samples, (n_samples, 1))
+        p_obs = Distribution(obs_samples, rv_name='$Q$', label='Observed')
+        cbayes_post_lf = CBayesPosterior(p_obs=p_obs, p_prior=p_prior, p_prior_pf=p_prior_pf_lf)
+        cbayes_post_lf.setup_posterior_and_pf()
+        print('Evaluating a low-fidelity posterior ...')
+        cbayes_post_lf.print_stats()
+        cbayes_post_lf.plot_results(2)
 
 # --------------------------------------------------------------------------- #